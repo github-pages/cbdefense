83ca27d2fb2dec209b77aa6aa438d1827fd97596
Matthew Morrison <matthewmorrison89@gmail.com>
2018-10-29 20:25:02 -0400

    Added better fail logging

--- cbdefense.py
+++ cbdefense.py
@@ -1,41 +1,38 @@
 #!/usr/bin/env python
 import requests, json, os, sys, re, socket, time
 from datetime import datetime
-from pandas.io.json import json_normalize
 
 def main():
     cbdefense_getevents()
 
 def cbdefense_getevents():
     headers = { 'X-Auth-Token' : os.environ['CBDEFENSE_API'] }
-    r = requests.get("https://api-prod05.conferdeploy.net/integrationServices/v3/event?searchWindow=3h&start=1&rows=2",headers=headers)
+    r = requests.get("https://api-prod05.conferdeploy.net/integrationServices/v3/event?searchWindow=1h&start=1&rows=1000000000",headers=headers)
     data = r.json()
     if r.status_code == requests.codes.ok:
         for each in data['results']:
             if 'blocked' in each['longDescription']:
-                if os.path.isfile("carbonblack.log"):
-                    with open('carbonblack.log', 'r+') as fi:
-                        text = fi.read()
-                        if re.search(each['eventId'], text):
-                            fi.close()
-                        else:
-                            for eventdetails in ['eventId']:
-                                event_url="https://api-prod05.conferdeploy.net/integrationServices/v3/event/" + each['eventId']
-                                r = requests.get(event_url, headers=headers)
-                                resp = r.content
-                                if r.status_code == requests.codes.ok:
-                                    jj = json.loads(resp)
-                                    j = json.dumps(jj)#.replace('\\n','').replace('\\"','"')
-                                    fi.write(j + '\r')
-                                    fi.close()
-                                else:
-                                    pass
-                else:
-                    pass
+                with open('carbonblack.log', 'a+') as fi:
+                    text = fi.read()
+                    if re.search(each['eventId'], text):
+                        fi.close()
+                    else:
+                        for eventdetails in ['eventId']:
+                            event_url="https://api-prod05.conferdeploy.net/integrationServices/v3/event/" + each['eventId']
+                            r = requests.get(event_url, headers=headers)
+                            resp = r.content
+                            if r.status_code == requests.codes.ok:
+                                jj = json.loads(resp)
+                                j = json.dumps(jj)#.replace('\\n','').replace('\\"','"')
+                                fi.write(j + '\r')
+                                fi.close()
+                            else:
+                                pass
             else:
                 pass
     else:
-        print("oh no, it failed")
-
-if __name__== "__main__":
+        with open('carbonblack_faillog.log', 'a+') as fi:
+            fi.write("Request returned status code ['%s'] because of reason ['%s']" % (r.status_code, r.reason.lower()))
+            fi.close()
+if __name__== '__main__':
       main()
